71e67c1552507e51f137f880ea23e2815c41bac5
Farhan <frashid2@ualberta.ca>
2024-05-29 18:11:47 -0600

    adding initial components

---
+++ tech-haven-e-commerce/src/components/ProductList.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+export default class ProductList extends Component {
+  render() {
+    return <h3>Hello from ProductList</h3>;
+  }
+}
