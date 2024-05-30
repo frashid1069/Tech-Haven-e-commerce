ead3cc2bee7b1397e0522dd0b8d745af56a8b76c
Farhan <frashid2@ualberta.ca>
2024-05-30 00:51:03 -0600

    adding navbar and its style

--- tech-haven-e-commerce/src/App.js
+++ tech-haven-e-commerce/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>Hello from the Tech Haven app</h3>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" Component={ProductList} />
+          <Route path="/details" Component={Details} />
+          <Route path="/cart" Component={Cart} />
+          <Route Component={Default} />
+          <Route />
+        </Switch>
       </React.Fragment>
     );
   }
